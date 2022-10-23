09678efd250584ff4654433e617ee41bc2b798d3
Arek Rado <arekrrr@gmail.com>
2022-10-23 23:31:43 +0700

    0.0.8

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# npm run lint:fix
+
+rm -rf dist/*
+./node_modules/typescript/bin/tsc --project tsconfig.build.json
+cp package.json LICENSE README.md dist
+# cd dist
+# npm publish --dry-run
